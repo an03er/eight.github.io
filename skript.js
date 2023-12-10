1772a5d633c7d5fc027aa0a5966adeb0a49e54f0
an03er <144229434+an03er@users.noreply.github.com>
2023-12-10 20:54:56 +0300

    Update skript.js

--- skript.js
+++ skript.js
@@ -1,24 +1,65 @@
  let button=document.getElementById("Button");
  let p=1;
- let name=document.getElementById("name");
+ var name=document.getElementsByName("name");
  let mail=document.getElementsByName("email")
  let tel=document.getElementsByName("tel")
  let massage=document.getElementsByName("massage")
- const handleEditClick = () => {
-    if(p==1){
-        document.querySelector(".wrapper-two").classList.remove("hide")
-        localStorage.setItem("name",name);
-        localStorage.setItem("mail",mail);
-        localStorage.setItem("tel",tel);
-        localStorage.setItem("massage",massage);
-        history.pushState({}, '', '/form/')
+ sessionStorage.setItem("is_reloaded", true);
+
+function showModal () {
+    history.pushState({form: true}, "", "./#form");
+    document.querySelector(".wrapper-two").classList.remove("hide")
+}
+function closeModal() {
+    history.replaceState({form: false}, "", "./");
+    document.querySelector(".wrapper-two").classList.add("hide");
+    document.querySelector(".wrapper-one").classList.remove("hide");
+}
+
+document.querySelector(".open").addEventListener("click", showModal)
+document.querySelector(".close").addEventListener("click", closeModal)
+
+function controlModal() {
+    if(location.hash == "#form") {
+        showModal();
     }
-    else{
-        document.querySelector(".wrapper-two").classList.add("hide")
-        document.querySelector(".wrapper-one").classList.remove("hide")
-        p=1
-        window.history.go(-1)
+    else {
+        closeModal();
     }
 }
-document.querySelector(".open").addEventListener("click", handleEditClick)
-document.querySelector(".close").addEventListener("click", handleEditClick)
+
+function loadInput(inputEl) {
+    inputEl.value = localStorage.getItem(inputEl.id);
+}
+
+function saveInput(event) {
+    localStorage.setItem(event.target.id, event.target.value);
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    if (history.state == null) {
+        history.pushState({form: false}, "", "./");
+    }
+    if (history.state.form) {
+        showModal();
+    }
+    
+    let feedbackButtonEl = document.getElementById("feedback-button");    
+    feedbackButtonEl.addEventListener('click', showModal);
+
+
+    let closeButton = document.getElementById("close-button");
+    closeButton.addEventListener("click", closeModal);
+
+    let inputEls = document.getElementsByClassName("inp");
+
+    [...inputEls].forEach(inputEl => {
+        loadInput(inputEl);
+    });
+    
+    [...inputEls].forEach(inputEl => {
+        inputEl.addEventListener("input", saveInput);
+    });
+
+    window.addEventListener("popstate", controlModal);
+});
